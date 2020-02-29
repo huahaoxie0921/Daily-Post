be76fd8c50ba4851c95ca3d1fe8121e64c2c7866
frankxie21 <huahaoxie@gmail.com>
2020-02-29 17:55:11 -0500

    app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
     console.log("The Daily Post Server Has Started!!!");
 });
 
